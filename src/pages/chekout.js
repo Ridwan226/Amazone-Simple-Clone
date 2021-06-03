d5c13cdab73464f2429bd110c903b4baa5d74b10
Ridwan226 <4515210063@univpancasila.ac.id>
2021-06-04 02:28:04 +0700

    Integrasi Stripe

--- src/pages/chekout.js
+++ src/pages/chekout.js
@@ -5,10 +5,35 @@ import Header from '../components/Header';
 import {selectItems, selectTotal} from '../slices/basketSlice';
 import Currency from 'react-currency-formatter';
 import {useSession} from 'next-auth/client';
+import {loadStripe} from '@stripe/stripe-js';
+import axios from 'axios';
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
+
 function Chekout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createdCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    // call checkout session
+    const checkoutSession = await axios.post('/api/crreate-checkout-session', {
+      items: items,
+      email: session.user.email,
+    });
+
+    // redirect success
+    // console.log(checkoutSession);
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    // if (result.error) {
+    //   alert(result.error.message);
+    // }
+  };
 
   return (
     <div className="bg-gray-100">
@@ -59,6 +84,8 @@ function Chekout() {
               </h2>
 
               <button
+                role="link"
+                onClick={createdCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
